98316588c19b4757a967f588efbfce7fd6d200eb
Nathan Schor <nate.schor@gmail.com>
2021-12-01 19:41:01 -0500

    return value instead of turning entries into T/F

--- code/R/03_prepare_for_modeling.R
+++ code/R/03_prepare_for_modeling.R
@@ -115,9 +115,10 @@ training_normalized_vars <- map_dfc(vars_to_normalize,
 training_normalized <- train_df %>% 
   select(-vars_to_normalize) %>% 
   bind_cols(., training_normalized_vars) %>% 
-  mutate(
+  filter(
     across(everything(), ~ !is.na(.))
   )
+  
 
 
 # Validation --------------------------------------------------------------
